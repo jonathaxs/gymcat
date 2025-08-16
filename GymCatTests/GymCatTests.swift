a91e936a43f87df4091cf424f27fa524d174252c
Jonathas Motta <jonathasmrt@gmail.com>
2025-08-16 20:25:26 -0300

    Initial Commit

---
+++ GymCatTests/GymCatTests.swift
@@ -0,0 +1,16 @@
+//
+//  GymCatTests.swift
+//  GymCatTests
+//
+//  Created by Jonathas Motta on 16/08/25.
+//
+
+import Testing
+
+struct GymCatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
