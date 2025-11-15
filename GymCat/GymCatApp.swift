8a29d50791e6576af47853f28b629f0d77530888
Jonathas Motta <jonathasmrt@gmail.com>
2025-11-15 00:29:08 -0300

    chore: set MainView as app entry point

--- GymCat/GymCatApp.swift
+++ GymCat/GymCatApp.swift
@@ -25,7 +25,7 @@ struct GymCatApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
         .modelContainer(sharedModelContainer)
     }
