a0967842f7f507e117f7511736d4ef352eb28e18
Jonathas Motta <jonathasmrt@gmail.com>
2025-12-19 14:47:06 -0300

    refactor: centralize default daily goals

--- GymCatApp/Helpers/DefaultGoals.swift
+++ GymCatApp/Helpers/DefaultGoals.swift
@@ -6,3 +6,16 @@
 //
 // ⌘
 
+import Foundation
+
+// MARK: - DefaultGoals
+// Centralizes the app's default daily goals.
+// In the future, these values should come from Settings (user preferences).
+enum DefaultGoals {
+    static let water: Int = 3000
+    static let protein: Int = 150
+    static let carbs: Int = 300
+    static let fats: Int = 80
+    static let creatine: Int = 6
+    static let sleep: Int = 7
+}
